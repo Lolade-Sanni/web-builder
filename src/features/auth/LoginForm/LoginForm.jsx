374b152897b0a74c7afb154e4be166a5302071b3
Lolade Sanni <loladesanni060@gmail.com>
2026-08-17 14:57:00 +0100

    chore: set up page files for auth

---
+++ src/features/auth/LoginForm/LoginForm.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+
+const LoginForm = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        console.log({
+            email,
+            password
+        });
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>
+                Email
+                <input
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                />
+            </label>
+
+            <label>
+                Password
+                <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                />
+            </label>
+
+            <button type="submit">
+                Login
+            </button>
+        </form>
+    );
+};
+
+export default LoginForm;
---
+++ src/pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,17 @@
+import LoginForm from "../features/auth/LoginForm.jsx";
+
+function LoginPage() {
+    return (
+        <main>
+            <div className="login-grid">
+                <div className='login-img'>
+                    <p>hello</p>
+                </div>
+                <LoginForm />
+            </div>
+
+        </main>
+    );
+}
+
+export default LoginPage;
